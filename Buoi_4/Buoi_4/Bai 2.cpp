32edec195a9663135df3deefae4b415cf6c47e15
nguyenthanhtien3214 <nguyenthanhtien3214@gmail.com>
2024-08-06 01:05:30 +0700

    Viết hàm tìm ước chung lớp nhất của 2 số nguyên dương a, b

---
+++ Buoi_4/Buoi_4/Bai 2.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+// Hàm tìm UCLN theo phương pháp đệ quy
+int gcd(int a, int b) {
+    if (a == b) {
+        return a;
+    }
+    else if (a > b) {
+        return gcd(a - b, b);
+    }
+    else {
+        return gcd(a, b - a);
+    }
+}
+
+int main() {
+    int a, b;
+    printf("Nhap hai so nguyen duong a va b: ");
+    scanf_s("%d %d", &a, &b);
+
+    printf("UCLN cua %d va %d la: %d\n", a, b, gcd(a, b));
+
+    return 0;
+}
