c08b3c49178aa0204055d236d967c190d8a2221c
nguyenthanhtien3214 <nguyenthanhtien3214@gmail.com>
2024-08-06 01:39:49 +0700

    Bài 7. Tìm số Fibonaci thứ n

---
+++ Buoi_4/Buoi_4/Bai 7.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+// Hàm đệ quy tính số Fibonacci thứ n
+int fibonacci(int n) {
+    if (n <= 2) {
+        return 1;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int n;
+    printf("Nhap chi so phan tu n: ");
+    scanf_s("%d", &n);
+
+    printf("So Fibonacci thu %d la: %d\n", n, fibonacci(n));
+
+    return 0;
+}
