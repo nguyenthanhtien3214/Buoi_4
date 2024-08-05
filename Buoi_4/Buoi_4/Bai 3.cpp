05845e898809b5aa0feed026504cf8ac5201a609
nguyenthanhtien3214 <nguyenthanhtien3214@gmail.com>
2024-08-06 01:08:33 +0700

    Bài 3. Viết hàm tìm giá trị phần tử thứ n của cấp số cộng có hạng đầu là a, công sai là r

---
+++ Buoi_4/Buoi_4/Bai 3.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+// Hàm đệ quy tìm giá trị phần tử thứ n của cấp số cộng
+int arithmetic_sequence_recursive(int a, int r, int n) {
+    if (n == 1) {
+        return a;
+    }
+    return arithmetic_sequence_recursive(a, r, n - 1) + r;
+}
+
+int main() {
+    int a, r, n;
+    printf("Nhap gia tri hang dau a: ");
+    scanf_s("%d", &a);
+    printf("Nhap gia tri cong sai r: ");
+    scanf_s("%d", &r);
+    printf("Nhap chi so phan tu n: ");
+    scanf_s("%d", &n);
+
+    printf("Gia tri phan tu thu %d cua cap so cong la: %d\n", n, arithmetic_sequence_recursive(a, r, n));
+
+    return 0;
+}
