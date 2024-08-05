e7468daf249343c51169fdbbbb23bde4153c7a1a
nguyenthanhtien3214 <nguyenthanhtien3214@gmail.com>
2024-08-06 01:34:57 +0700

    Bài 6. Đếm số chữ số nguyên dương n.

---
+++ Buoi_4/Buoi_4/Bai 6.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+// Hàm đệ quy đếm số chữ số của một số nguyên dương
+int count_digits(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    return 1 + count_digits(n / 10);
+}
+
+int main() {
+    int n;
+    printf("Nhap mot so nguyen duong n: ");
+    scanf_s("%d", &n);
+
+    // Kiểm tra nếu n là 0 thì số chữ số là 1
+    if (n == 0) {
+        printf("So chu so cua %d la: 1\n", n);
+    }
+    else {
+        printf("So chu so cua %d la: %d\n", n, count_digits(n));
+    }
+
+    return 0;
+}
